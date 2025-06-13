7470bcaf32e40d861d61916e2e50ff98ee8e5da5
mo <mouhamedbocar@yahoo.fr>
2025-06-13 11:58:32 +0800

    desktop config for the sourcepro

---
+++ sourcepro/config/desktop.py
@@ -0,0 +1,35 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Sourcing Request",
+            "label": _("Sourcing Request"),
+            "color": "#34a853",
+            "icon": "octicon octicon-package",
+            "type": "module",
+            "description": _("Manage sourcing item requests and supplier follow-up."),
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Sourcing Request",
+                    "label": _("Sourcing Request"),
+                },
+                {
+                    "type": "doctype",
+                    "name": "Requested Item",
+                    "label": _("Requested Item"),
+                },
+                {
+                    "type": "doctype",
+                    "name": "Supplier Info",
+                    "label": _("Supplier Info"),
+                },
+                {
+                    "type": "doctype",
+                    "name": "Sourcing Quotation",
+                    "label": _("Sourcing Quotation"),
+                }
+            ]
+        }
+    ]
